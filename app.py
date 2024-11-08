fc0d126e974b64a912d12fbc74237eb8841ffba1
JayMehta21 <jaykalpeshmehta@gmail.com>
2024-11-08 16:15:38 +0530

    Fixed route to return 'Hello--World!' to match the test

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask 
+from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
- return "Hello, World!"
-
-
+    return "Hello--World!"  # Change this to match the expected output in the test
 
 if __name__ == "__main__":
-        app.run(host="0.0.0.0",port=5000)
+    app.run(host="0.0.0.0", port=5000)
